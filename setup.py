60f12f917e6d33b21be915e2852611937121ff5f
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2016-04-18 09:56:51 +0900

    update setup.py for distribution

--- setup.py
+++ setup.py
@@ -3,27 +3,38 @@
 
 from setuptools import setup, find_packages
 
-with open('jubakit/_version.py') as f:
-  exec(f.read())
+def _read(filename):
+  with open(filename) as f:
+    return f.read()
+
+# Load package version.
+exec(_read('jubakit/_version.py'))
 
 setup(name='jubakit',
       version='.'.join(str(x) for x in VERSION),
       description='Jubatus Toolkit',
+      long_description=_read('README.rst'),
+      url='http://jubat.us',
       author='PFN & NTT',
       author_email='jubatus-team@googlegroups.com',
-      url='http://jubat.us',
-      license='MIT License',
+      license='MIT',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+      ],
       packages=find_packages(exclude=['jubakit.test']),
       install_requires=[
           'jubatus>=0.8.0',
           'psutil',
-      ],
-      classifiers=[
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          'Topic :: Scientific/Engineering :: Information Analysis'
       ],
       test_suite = 'jubakit.test',
 )
