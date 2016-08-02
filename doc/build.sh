8d7b83c1f6edcadb327ec12a378f7f3b8dd5ba1b
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2016-08-02 11:52:34 +0900

    fix build command to generate non-separated docs

--- doc/build.sh
+++ doc/build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-sphinx-apidoc -f -M -e -H "API Reference" -o source ../jubakit ../jubakit/test
+sphinx-apidoc -f -M -H "API Reference" -o source ../jubakit ../jubakit/test
+
+# A dirty workaround to remove unnecessary "Subpackages" / "Submodules" header...
+for FILE in source/jubakit*.rst; do
+  perl -pi -e 'BEGIN {undef $/;} s/Sub(packages|modules)\n\-+\n//g' "${FILE}"
+done
+
 PYTHONPATH=.. make html
