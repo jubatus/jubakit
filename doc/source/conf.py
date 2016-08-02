99d95c6282972e1553a4c9131081d4946a21ea22
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2016-08-02 12:18:56 +0900

    fix to generate docs for __init__ method

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -428,3 +428,12 @@ epub_exclude_files = ['search.html']
 # If false, no index is generated.
 #
 # epub_use_index = True
+
+# Autodoc
+def autodoc_skip_handler(app, what, name, obj, skip, options):
+    if name == "__init__":
+        return False
+    return skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", autodoc_skip_handler)
