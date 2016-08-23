807a3a833014097b264df374f4221f73574cdc10
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2016-08-23 19:39:24 +0900

    fix all options work universally on Python 2.x and 3.x

--- jubakit/loader/csv.py
+++ jubakit/loader/csv.py
@@ -30,28 +30,14 @@ class CSVLoader(BaseLoader):
     >>> loader = CSVLoader('dataset.tsv', fieldnames=False, encoding='cp932', delimiter='\t')
     """
 
-    # Some dialect parameters must be encoded in unicode on Py3, whereas
-    # bytes on Py2.  We conceal such differences.
-    for cp in ['delimiter', 'escapechar', 'quotechar']:
-      if cp not in kwargs: break
-
-      v = kwargs[cp]
-      if PYTHON3 and isinstance(v, bytes):
-        v = v.decode(encoding)
-      elif not PYTHON3 and isinstance(v, unicode_t):
-        v = v.encode(encoding)
-      kwargs[cp] = v
-
     if fieldnames == True:
       # Automatically estimate field names later.
       fieldnames = None
     elif fieldnames == False:
-      # Generate field names by peeking number of columns in the first row
-      # of the CSV.
+      # Generate field names by peeking number of columns in the first row of the CSV.
       with io.open(filename, encoding=encoding, newline='') as f:
         # Use fieldnames from DictReader to count number of columns in the first row.
-        # We use csv.DictReader instead of plain csv.reader for compatibility of *args and **kwargs.
-        r = csv.DictReader(f, fieldnames=None, *args, **kwargs)
+        r = _UnicodeDictReader(f, encoding, fieldnames=None, *args, **kwargs)
         fieldnames = ['c{0}'.format(i) for i in range(len(r.fieldnames))]
 
     self._filename = filename
@@ -61,14 +47,44 @@ class CSVLoader(BaseLoader):
     self._kwargs = kwargs
 
   def rows(self):
-    def _encode_file(f, enc):
-      for line in f: yield line.encode(enc)
-    def _decode_row(r, enc):
-      return dict([(k.decode(enc), r[k].decode(enc)) for k in r.keys()])
-
     with io.open(self._filename, encoding=self._encoding, newline='') as f:
-      f = f if PYTHON3 else _encode_file(f, self._encoding)
-      reader = csv.DictReader(f, fieldnames=self._fieldnames, *self._args, **self._kwargs)
+      reader = _UnicodeDictReader(f, self._encoding, fieldnames=self._fieldnames, *self._args, **self._kwargs)
       for row in reader:
-        ent = row if PYTHON3 else _decode_row(row, self._encoding)
-        yield ent
+        yield row
+
+class _UnicodeDictReader(csv.DictReader):
+  def __init__(self, f, encoding, *args, **kwargs):
+    self._encoding = encoding
+
+    # DictReader in Python 2.x use str (bytes) for parameters, whereas Python 3.x use
+    # str (unicode) for them.  The code below is intended to absorbs the difference.
+    def convert(v):
+      if PYTHON3 and isinstance(v, bytes):
+        return v.decode(encoding)
+      elif not PYTHON3 and isinstance(v, unicode_t):
+        return v.encode(encoding)
+      return v
+
+    for k in ['delimiter', 'escapechar', 'quotechar', 'lineterminator', 'restkey', 'restval']:
+      if k in kwargs: kwargs[k] = convert(kwargs[k])
+
+    fieldnames = kwargs.get('fieldnames', None)
+    if fieldnames:
+      kwargs['fieldnames'] = list(map(lambda x: convert(x), fieldnames))
+
+    # DictReader in Python 2.x cannot handle Unicode input.
+    # We transcode each line of CSV row into bytes for Py2.
+    def encode_file(f):
+      for line in f:
+        yield line.encode(encoding)
+    f = f if PYTHON3 else encode_file(f)
+    csv.DictReader.__init__(self, f, *args, **kwargs)
+
+  def next(self):
+    r = csv.DictReader.next(self)
+    if PYTHON3:
+      return r
+    else:
+      # DictReader in Python 2.x returns rows in bytes.  We transcode keys/values of
+      # the row dict into Unicode like in Python 3.x.
+      return dict([(k.decode(self._encoding), r[k].decode(self._encoding)) for k in r.keys()])
